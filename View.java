f9ed2157152123f4697c46d62528febcf586307c
Simon Hanisch <hanischsimon@gmail.com>
2014-09-20 16:48:49 +0200

    Now I realy added the UI

---
+++ View.java
@@ -0,0 +1,145 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import java.io.File;
+
+public class View extends Application{
+
+	final CSVReader reader = new CSVReader();
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+
+		GridPane grid = new GridPane();
+
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25,25,25,25));
+
+		Text scenetitle = new Text("Welcome to Adam 5.0");
+		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		grid.add(scenetitle,0,0,2,1);
+
+		
+		//Button for Data selection
+		FileChooser dataChooser = new FileChooser();
+		dataChooser.setTitle("Select Data");
+		
+		Button dataBtn = new Button("Select Data");
+		grid.add(dataBtn,0,1);
+
+		TextField dataTextField = new TextField();
+		grid.add(dataTextField,1,1); 
+
+		dataBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e){
+				File file = dataChooser.showOpenDialog(primaryStage);
+				if (file != null){
+					dataTextField.setText(file.getPath());
+				}
+			}
+
+		});
+
+		
+		//Button for Question selection
+		Button questionBtn = new Button("Select Questions");
+		grid.add(questionBtn,0,2);
+
+		TextField questionTextField = new TextField();
+		grid.add(questionTextField,1,2);
+
+		questionBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e){
+				questionTextField.setText("Selected");
+			}
+
+		});
+
+
+		//Button for Report selection
+		Button reportBtn = new Button("Select Report");
+		grid.add(reportBtn,0,3);
+
+		TextField reportTextField = new TextField();
+		grid.add(reportTextField,1,3);
+
+		reportBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e){
+				reportTextField.setText("Selected");
+			}
+
+		});
+
+		
+		//Save Button bottom right corner. 
+		Button saveBtn = new Button("Save Diagram");
+		HBox hbSaveBtn = new HBox(10);
+		hbSaveBtn.setAlignment(Pos.BOTTOM_RIGHT);
+		hbSaveBtn.getChildren().add(saveBtn);
+		grid.add(hbSaveBtn,1,4);
+
+		final Text actiontarget = new Text();
+		grid.add(actiontarget,1,6);
+
+		saveBtn.setOnAction(new EventHandler<ActionEvent>() { 
+
+			@Override
+			public void handle(ActionEvent e){
+				actiontarget.setFill(Color.FIREBRICK);
+				actiontarget.setText("Diagrams generated.");
+				
+				if (dataTextField.getText().endsWith(".CSV") || dataTextField.getText().endsWith(".csv") ){
+					reader.insertCSVFile("test1",dataTextField.getText()); //TODO: Generate Tablename from file path
+				}
+				else {
+					dataTextField.setText("Not a csv File!");
+				}
+			}
+		});
+
+
+
+
+
+		//grid.setGridLinesVisible(true);
+		Scene scene = new Scene(grid,300,275);
+		primaryStage.setTitle("Adam 5.0");
+		primaryStage.setScene(scene);
+		//primaryStage.getStylesheets().add(Login.class.getResource("Adam.css").toExternalForm());  //TODO: Pimp my form!
+		primaryStage.show();
+	}
+
+	//TODO: Write a function that validates if a file realy exists.
+	private Boolean validatePath(String pathString){
+
+	}
+
+	public static void main(String[] args){
+		launch(args); // TODO: I have to include the JavaFX Packager to embeded the JavaFX Launcher
+	}
+
+}
